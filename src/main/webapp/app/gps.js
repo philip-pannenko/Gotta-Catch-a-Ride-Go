dcc3b1ed54487720617b1db0bcca07b6e6f2b679
Liz Hesser <Liz.Hesser@dealetrack.com>
2016-11-12 11:50:33 -0500

    initial check in - gps data grab

---
+++ src/main/webapp/app/gps.js
@@ -0,0 +1,25 @@
+
+var container = document.getElementById("data-view");
+var latitude = document.getElementById("latitude");
+var longitude = document.getElementById("longitude");
+var accuracy = document.getElementById("accuracy");
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	}
+	else {
+		container.innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
+function showPosition(position) {
+	if (position && position.coords) {
+		latitude.innerHTML = position.coords.latitude;
+		longitude.innerHTML = position.coords.longitude;
+		accuracy.innerHTML = position.coords.accuracy;
+	}
+}
+
+// while(true) {
+	getLocation();
+// }
